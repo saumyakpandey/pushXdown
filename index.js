58779398a2b37306d9c29c19840e9459b988c787
Saumyak Pandey <55388802+saumyakpandey@users.noreply.github.com>
2019-09-22 18:07:40 +0530

    Added screens in HTML and greetings on start

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ button.onclick = function () {
     button.innerHTML = "Push UP: " + count;
 };
 
+// function gdPrac() {
+//     if (count = 3) {
+//         alert("Rabbit number " + i);
+//         // else {
+
+//         // }
+//     }
+// };
+
+source.onopen = function () {
+    var time = new Date().getHours();
+    var greeting;
+    if (time < 20) {
+        greeting = "Good day";
+    } else {
+        greeting = "Good evening";
+    }
+    alert(greeting);
+}
 
 async function registerSW() {
     if ('serviceWorker' in navigator) {
