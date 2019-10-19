8e9edb61c1b9de3419cb4b5c8523165c786e60b7
Saumyak Pandey <55388802+saumyakpandey@users.noreply.github.com>
2019-10-19 16:20:54 +0530

    Added new functionality.

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ button.onclick = function () {
     button.innerHTML = "Push UP: " + count;
 };
 
+// function gdPrac() {
+//     if (count = 3) {
+//         alert("Rabbit number " + i);
+//         // else {
+
+//         // }
+//     }
+// };
+
+window.onload = function () {
+    var today = new Date();
+    var timeDisp = today.getHours() + ":" + today.getMinutes();
+    var time = new Date().getHours();
+    var greeting;
+    if (time < 20) {
+        greeting = "Good day";
+    } else {
+        greeting = "Good evening";
+    }
+    document.getElementById("timing").innerHTML = greeting + timeDisp;
+}
 
 async function registerSW() {
     if ('serviceWorker' in navigator) {
